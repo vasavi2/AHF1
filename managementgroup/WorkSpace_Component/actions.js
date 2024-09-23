0e6b055f94ba5f005ba4b24191420338e03bcc94
vasavi2 <54984019+vasavi2@users.noreply.github.com>
2024-09-23 16:55:09 +0530

    Delete managementgroup directory

--- managementgroup/WorkSpace_Component/actions.js
+++
@@ -1,5 +0,0 @@
-import { createAction } from 'redux-actions';
-import * as Actions from './constants';
-export const editorUpdatesAction = createAction(Actions.EDITOR_UPDATES);
-export const propertiesUpdatesAction = createAction(Actions.EDITOR_PROPERTIES);
-export const workspaceCreateAction = createAction(Actions.CREATE_WORKSPACE);
