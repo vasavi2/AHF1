f249080792fd1e2ad8d9809dd9945878828f2731
vasavi2 <54984019+vasavi2@users.noreply.github.com>
2024-09-23 16:52:05 +0530

    Add files via upload

---
+++ frontend/src/PowerBi.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PowerBI from './PowerBi_Component/PowerBI';
+const PowerBi = () => {
+    return (
+        <div>
+            <PowerBI/>
+        </div>
+    );
+};
+
+export default PowerBi;
---
+++ frontend/src/Workspace.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Tab2 from './WorkSpace_Component/Tab2';
+
+const Workspace = () => {
+    return (
+        <div >
+            <Tab2/>
+        </div>
+
+        
+    );
+};
+
+export default Workspace;
