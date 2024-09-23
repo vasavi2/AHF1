6e700ea1266a0e3e8d874c53b64c89fae972a1af
vasavi2 <54984019+vasavi2@users.noreply.github.com>
2024-09-23 16:56:36 +0530

    Add files via upload

---
+++ frontend/management_component/Management_Component/Sample.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+// import Header from './Header';
+import NewPopup from './PopupAlert';
+const Sample = () => {
+    return (
+        <div>
+            <Header/>
+            <NewPopup/>
+            
+        </div>
+    );
+};
+
+export default Sample;
